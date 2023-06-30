870008b676b8226fb6b1446e5a8a2969d198891a
Harry <40882986+HarryHyman@users.noreply.github.com>
2023-06-30 14:18:44 +0100

    Separate out ingredients, instructions and notes

--- app.py
+++ app.py
@@ -5,8 +5,23 @@ site = requests.get("https://www.janespatisserie.com/2019/05/30/no-bake-lemon-ch
 
 soup = BeautifulSoup(site, "html.parser")
 
-recipe = soup.find(class_="wprm-recipe")
+# recipe = soup.find(class_="wprm-recipe")
+# recipe_text = recipe.get_text(separator="")
 
-recipe_text = recipe.get_text(separator="")
+ingredients = soup.find(class_="wprm-recipe-ingredients-container")
+ingredients_text = soup.get_text(separator="")
 
-print(recipe_text)
+instructions = soup.find(class_="wprm-recipe-instructions-container")
+instructions_text = soup.get_text(separator="")
+
+notes = soup.find(class_="wprm-recipe-notes-container")
+notes_text = soup.get_text(separator="")
+
+print("*** INGREDIENTS ***")
+print(ingredients_text)
+
+print("*** INSTRUCTIONS ***")
+print(instructions_text)
+
+print("*** NOTES ***")
+print(notes_text)
