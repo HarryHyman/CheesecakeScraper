90018c0884618fd7a84de7985cd2c9dc0d0f0fe8
Harry <40882986+HarryHyman@users.noreply.github.com>
2023-06-30 14:09:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import requests
+from bs4 import BeautifulSoup
 
-site = requests.get("https://www.janespatisserie.com/2019/05/30/no-bake-lemon-cheesecake/")
+site = requests.get("https://www.janespatisserie.com/2019/05/30/no-bake-lemon-cheesecake/").text
 
-print(site)
+soup = BeautifulSoup(html_content, "html.parser")
+
+recipe = soup.find(class="wprm-recipe")
+
+print(recipe)
